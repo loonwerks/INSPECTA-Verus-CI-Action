1a4b428e72d00cf79c40d128c19b68ef42bad6fa
Karl Hoech <karl.hoech@collins.com>
2026-02-09 16:55:41 -0600

    Initial implementation

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # INSPECTA-Verus-CI-Action
+
 INSPECTA CI action to conduct analysis of Rust implementation code using Verus
+
+## Inputs
+
+### `sourcepath`
+
+Path to top level Makefile (expects path string).
+
+### `environment-variables`
+
+JSON-formatted dictionary of environment variables to pass to the make system.
+
+## Outputs
+
+## `result`
+
+The JSON-formatted summary of analysis results.
+
+## Example usage
+
+uses: actions/INSPECTA-Verus-CI-Action@v1
+with:
+  sourcepath: 'system/hamr/microkit'
---
+++ entrypoint.sh
@@ -0,0 +1,40 @@
+#!/bin/bash -l
+
+echo "sourcepath: $1"
+echo "environment-variables: $2"
+
+runCommand=(/Sireum/bin/sireum hamr sysml codegen)
+
+sourcePath=system/hamr/microkit
+if [[ -n $1 ]]; then
+	sourcePath=$1
+fi
+
+environmentVariables=""
+if [[ -n $2 ]]; then
+	environmentVariables=$(echo $2 | jq -r '[to_entries[] | .key + "=" + (.value | tostring)] | join(" ")')
+fi
+
+# SysMLv2 files
+runCommand+=(pushd $GITHUB_WORKSPACE/${sourcePath} \&\& environmentVariables make verus \&\& popd)
+
+outputFile="codegen.out"
+if [[ -n $3 ]]; then
+	outputFile=$3
+fi
+
+echo "run command: ${runCommand[@]}" 
+
+"${runCommand[@]}" >> "$outputFile"
+EXIT_CODE=$?
+
+echo "timestamp=$(date)" >> $GITHUB_OUTPUT
+echo "status=${EXIT_CODE}" >> $GITHUB_OUTPUT
+echo "status-messages=$(cat ${outputFile} | jq -R -s '.')" >> $GITHUB_OUTPUT
+
+echo "exit code: $EXIT_CODE"
+if [ "XX $EXIT_CODE" = "XX 0" ]; then
+	exit 0
+else
+	exit 1
+fi
